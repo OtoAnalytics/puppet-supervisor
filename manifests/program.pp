b297d00b60c80d6194172ca73e42b870f672ec44
Zach Leslie <zleslie@womply.com>
2016-05-02 10:56:27 -0700

    Add define documentation
    Without this change, users of this module don't have documentation or an
    example of how the supervisord::program define is meant to be used.

--- manifests/program.pp
+++ manifests/program.pp
@@ -1,6 +1,15 @@
+# This define manages a supervisor program
+#
+# @example
+#   supervisord::program { 'puppetboard':
+#     command => "${virtualenv_path}/bin/uwsgi --socket ${sock} --wsgi-file ${wsgi_file} -H ${virtualenv_path} --chown-socket=nginx:nginx",
+#     user    => 'nginx',
+#     require => File['puppetboard.wsgi']
+#   }
+#
 define supervisord::program (
   $command,
-  $user = undef,
+  $user        = undef,
   $environment = undef,
 ) {
   include supervisord
